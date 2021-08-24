9e866788853e30841134f7b1f304decab5691337
Lucas da Silva Lima <lima.1996@alunos.utfpr.edu.br>
2021-08-24 16:58:56 -0400

    Fix some bugs

--- fuzzy/app.py
+++ fuzzy/app.py
@@ -3,4 +3,4 @@ from fuzzy_api import server
 app = server.app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- fuzzy/start.sh
+++ fuzzy/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t flasgger .
-docker run -it --rm -p 5000:5000 --name flasgger flasgger
+docker run -it --rm -p 5001:5000 --name flasgger flasgger
